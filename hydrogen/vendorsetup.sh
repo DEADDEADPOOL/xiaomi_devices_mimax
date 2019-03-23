8370ff4c8e19910aca2e0d5d1ef5821fe2a13f4f
Rohit <46891277+DEADDEADPOOL@users.noreply.github.com>
2019-03-23 20:51:42 +0530

    Update vendorsetup.sh

--- hydrogen/vendorsetup.sh
+++ hydrogen/vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo nitrogen_hydrogen-userdebug
-add_lunch_combo nitrogen_hydrogen-eng
+add_lunch_combo dot_hydrogen-userdebug
+add_lunch_combo dot_hydrogen-eng
